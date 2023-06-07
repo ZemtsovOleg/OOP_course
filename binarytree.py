5c4e4ded0911b69fe49fd5665552b8b97a397dbc
OlegZemtsov <oleg9175495330@gmail.com>
2023-06-07 14:05:17 +0400

    new file

--- binarytree.py
+++ binarytree.py
@@ -1,111 +1,93 @@
 class Node:
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, val) -> None:
+        self.val = val
         self.left = None
         self.right = None
 
 
 class BinaryTree:
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         self.root = None
-        for key in args:
-            self.insert(key)
+        for val in args:
+            self.insert(val)
 
-    def insert(self, key) -> None:
+    def insert(self, val) -> None:
         if self.root is None:
-            self.root = Node(key)
+            self.root = Node(val)
         else:
-            self._insert_recursive(self.root, key)
+            self.__insert_recursive(self.root, val)
 
-    def _insert_recursive(self, node: Node, key) -> None:
-        if key < node.key:
+    def __insert_recursive(self, node: Node, val) -> None:
+        if val < node.val:
             if node.left is None:
-                node.left = Node(key)
+                node.left = Node(val)
             else:
-                self._insert_recursive(node.left, key)
-        if key > node.key:
+                self.__insert_recursive(node.left, val)
+        if val > node.val:
             if node.right is None:
-                node.right = Node(key)
+                node.right = Node(val)
             else:
-                self._insert_recursive(node.right, key)
+                self.__insert_recursive(node.right, val)
 
-    # def show_tree(self, node=1):
-    #     if node == 1:
-    #         node = self.root
-    #     if node is None:
-    #         return
-    #     self.show_tree(node.left)
-    #     print(node.key, end=' ')
-    #     self.show_tree(node.right)
-    #
-    # def show_wide_tree(self, node=1):
-    #     if node == 1:
-    #         node = self.root
-    #     if node is None:
-    #         return None
-    #     v = [node]
-    #     while v:
-    #         vn = []
-    #         for x in v:
-    #             print(x.key, end=' ')
-    #             if x.left:
-    #                 vn += [x.left]
-    #             if x.right:
-    #                 vn += [x.right]
-    #         v = vn
-    #         print()
+    def show_tree(self, node):
+        if node is None:
+            return
+        self.show_tree(node.left)
+        print(node.val, end=' ')
+        self.show_tree(node.right)
 
-    def find_max_branch_sum(self) -> int:
-        if self.root is None:
+    def show_wide_tree(self, node):
+        if node is None:
             return None
-
-        max_sum = float('-inf')
-
-        def max_branch_sum(node):
-            nonlocal max_sum
-
-            if node is None:
-                return 0
-
-            left_sum = max_branch_sum(node.left)
-            right_sum = max_branch_sum(node.right)
-
-            max_sum = max(max_sum, node.key + max(left_sum, right_sum))
-            return node.key + max(left_sum, right_sum)
-
-        max_branch_sum(self.root)
-        return max_sum
+        v = [node]
+        while v:
+            vn = []
+            for x in v:
+                print(x.val, end=' ')
+                if x.left:
+                    vn += [x.left]
+                if x.right:
+                    vn += [x.right]
+            v = vn
+            print()
 
 
-one = BinaryTree(10, 5, 6, 13, 16, 11, 2)
+    # def find_max_branch_sum(self) -> int:
+    #     if self.root is None:
+    #         return None
 
-print(one.find_max_branch_sum())
-# one.show_tree()
-# one.show_wide_tree()
+    #     max_sum = float('-inf')
 
+    #     def max_branch_sum(node):
+    #         nonlocal max_sum
 
-# def max_path_sum(node):
-#     if node is None:
-#         return None
-#     max_path_sum(node.left)
-#     max_path_sum(node.right)
-#     return
+    #         if node is None:
+    #             return 0
 
-# print(max_path_sum(one.root))
+    #         left_sum = max_branch_sum(node.left)
+    #         right_sum = max_branch_sum(node.right)
 
+    #         max_sum = max(max_sum, node.key + max(left_sum, right_sum))
+    #         return node.key + max(left_sum, right_sum)
 
-# def rec(value):
-#     print(value)
-#     if value < 5:
-#         rec(value+1)
-#     print(value)
-
-# rec(1)
+    #     max_branch_sum(self.root)
+    #     return max_sum
 
 
-# def fac_rec(n):
-#     if n < 2:
-#         return 1
-#     return n * fac_rec(n-1)
+one = BinaryTree(10, 5, 6, 13, 16, 11, 2, 3, 12)
+two = BinaryTree(1, 5, 6, 13, 16, 9, 2, 3, 12)
 
-# print(fac_rec(5))
+# print(one.find_max_branch_sum())
+one.show_tree()
+one.show_wide_tree()
+
+
+def compare(a, b):
+    if a is None or b is None:
+        return a is b
+    return a.val == b.val and compare(a.left, b.left) and compare(a.right, b.right)
+
+
+print(compare(one.root, two.root))
+
+
--- treenode_max_branch_sum.py
+++ treenode_max_branch_sum.py
@@ -1,16 +1,13 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def hasPathSum(self, root, targetSum: int) -> bool:
-        pass
-
-
-    def find_max_branch_sum(self) -> int:
-        if self.root is None:
+        if root is None:
             return None
 
         max_sum = float('-inf')
@@ -24,8 +21,8 @@ class Solution:
             left_sum = max_branch_sum(node.left)
             right_sum = max_branch_sum(node.right)
 
-            max_sum = max(max_sum, node.key + max(left_sum, right_sum))
-            return node.key + max(left_sum, right_sum)
+            max_sum = max(max_sum, node.val + max(left_sum, right_sum))
+            return node.val + max(left_sum, right_sum)
 
-        max_branch_sum(self.root)
+        max_branch_sum(root)
         return max_sum
